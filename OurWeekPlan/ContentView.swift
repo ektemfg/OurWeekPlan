12c6fbab72f6dc59d1d81b3e246344764d7a7fd8
ektemfg <ekteg@icloud.com>
2022-10-25 23:26:12 +0200

    Initial Commit

---
+++ OurWeekPlan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OurWeekPlan
+//
+//  Created by Dimitrije Pesic on 25/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OurWeekPlan/OurWeekPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  OurWeekPlanApp.swift
+//  OurWeekPlan
+//
+//  Created by Dimitrije Pesic on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct OurWeekPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
